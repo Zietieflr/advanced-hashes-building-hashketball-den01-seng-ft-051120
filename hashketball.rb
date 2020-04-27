47cf185a941790087aa43abdafe1a4b8862dea6a
Logan McGuire <lrmcguire93@gmail.com>
2020-04-27 19:39:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -94,7 +94,7 @@ def game_hash
           slam_dunks: 10
         },
         {
-          player_name: "DeSangna Diop",
+          player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
           points: 24,
